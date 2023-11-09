53eff1fd48718a4f37a27aedd317e2f065f0ff27
unknown <lalalapienroje@gmail.com>
2023-11-09 09:26:58 +0700

    profile

---
+++ lib/Widget/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePict extends StatefulWidget {
+  const ProfilePict({super.key});
+
+  @override
+  State<ProfilePict> createState() => _ProfilePictState();
+}
+
+class _ProfilePictState extends State<ProfilePict> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/register.dart
+++ lib/register.dart
@@ -41,118 +41,128 @@ class _RegisterState extends State<Register> {
             child: Container(
               color: Colors.white,
               child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: screenWidth * 0.82,
-                        height: screenHeight * 0.06,
-                        child: const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Daftar Akun',
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: screenWidth * 0.82,
+                      height: screenHeight * 0.10,
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Daftar Akun',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    myTextField(controller.emailController, 'Email', false,
+                        TextInputType.emailAddress, Icons.email_rounded),
+                    SizedBox(
+                      width: screenWidth * 0.82,
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Lupa Password ?",
                             style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold
+                              color: Color(0xFF77B2DE),
                             ),
                           ),
                         ),
                       ),
-                      myTextField(
-                          controller.emailController,
-                          'Email',
-                          false,
-                          TextInputType.emailAddress,
-                          Icons.email_rounded
-                      ),
-                      SizedBox(
-                        width: screenWidth * 0.82,
-                        child: Align(
-                          alignment: Alignment.topRight,
-                          child: TextButton(
-                            onPressed: () {
-                            },
-                            child: const Text(
-                              "Lupa Password ?",
-                              style: TextStyle(
-                                color: Color(0xFF77B2DE),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: isButtonEnabled() ? () => {
-                          Get.to(() => const Register2())
-                        } : null,
-                        style: ElevatedButton.styleFrom(
-                          disabledBackgroundColor: const Color(0xFFA8A8A8),
-                          foregroundColor: Colors.white, backgroundColor: const Color(0xFF0D5D97), shape: RoundedRectangleBorder(
+                    ),
+                    ElevatedButton(
+                      onPressed: isButtonEnabled()
+                          ? () => {Get.to(() => const Register2())}
+                          : null,
+                      style: ElevatedButton.styleFrom(
+                        disabledBackgroundColor: const Color(0xFFA8A8A8),
+                        foregroundColor: Colors.white,
+                        backgroundColor: const Color(0xFF0D5D97),
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
-                          minimumSize: Size(screenWidth * 0.7, 43),
+                        minimumSize: Size(screenWidth * 0.7, 43),
+                      ),
+                      child: Text(
+                        'Daftar',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color:
+                              isButtonEnabled() ? Colors.white : Colors.white,
                         ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: screenWidth * 0.82,
+                      height: screenHeight * 0.06,
+                      child: const Center(
                         child: Text(
-                          'Daftar',
+                          'Daftar dengan',
                           style: TextStyle(
-                            fontSize: 15,
-                            color: isButtonEnabled() ? Colors.white : Colors.white,
+                            color: Color(0xFF9F9F9F),
+                            fontSize: 11,
                           ),
                         ),
                       ),
-                      SizedBox(
-                        width: screenWidth * 0.82,
-                        height: screenHeight * 0.06,
-                        child: const Center(
-                          child: Text(
-                            'Daftar dengan',
-                            style: TextStyle(
-                              color: Color(0xFF9F9F9F),
-                              fontSize: 11,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buttonGoBuk(
-                              null,
-                              screenWidth,
-                              const Text("Google", style: TextStyle(color: Colors.black,fontSize: 11)),
-                              Image.asset("assets/images/icons_images/devicon_google.png",width: 20,height: 20,)
-                          ),
-                          buttonGoBuk(null,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buttonGoBuk(
+                            null,
                             screenWidth,
-                            const Text("Facebook", style: TextStyle(color: Colors.black,fontSize: 11)),
-                            const Icon(Icons.facebook, color: Colors.blue,),
+                            const Text("Google",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 11)),
+                            Image.asset(
+                              "assets/images/icons_images/devicon_google.png",
+                              width: 20,
+                              height: 20,
+                            )),
+                        buttonGoBuk(
+                          null,
+                          screenWidth,
+                          const Text("Facebook",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 11)),
+                          const Icon(
+                            Icons.facebook,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: screenWidth * 0.82,
+                      height: screenHeight * 0.06,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Sudah punya akun?",
+                            style: TextStyle(fontSize: 11),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Get.off(() => const Login());
+                            },
+                            child: const Text(
+                              "Masuk Sekarang !",
+                              style: TextStyle(
+                                color: Color(0xFF609BC6),
+                                fontSize: 11,
+                              ),
+                            ),
                           ),
                         ],
                       ),
-                      SizedBox(
-                        width: screenWidth * 0.82,
-                        height: screenHeight * 0.06,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text("Sudah punya akun?", style: TextStyle(fontSize: 11),),
-                            TextButton(
-                              onPressed: () {
-                                Get.off(() => const Login());
-                              },
-                              child: const Text(
-                                "Masuk Sekarang !",
-                                style: TextStyle(
-                                  color: Color(0xFF609BC6),
-                                  fontSize: 11,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
